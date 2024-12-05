3391043e7cd1e69a1d5d2b8e264e0f30a6674a5d
Abdallah <abdallah21basemen@gmail.com>
2024-12-05 13:49:14 +0200

    add new schema to catigories of brands in DB and make some enhancement in code to make it better and add some fetures read it in new file .eslintrc.json and in t.txt

---
+++ models/subCategoryModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const subCategorySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,//add word to DB without spaces before or after words
+            unique: [true, 'SubCategory must be unique'],
+            minlength: [2, 'To short SubCategory name'],
+            maxlength: [32, 'To long SubCategory name'],
+        },
+        slug: {
+            type: String,
+            lowercase: true,
+        },
+        category: {//demond to the main schema of catigory thet i made previous
+            type: mongoose.Schema.ObjectId,
+            ref: 'Category',
+            required: [true, 'SubCategory must be belong to parent category'],
+        },
+    },{ timestamps: true });
+
+module.exports = mongoose.model('SubCategory', subCategorySchema);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
+
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 //====================================================================================================
 // express app
 const PORT = process.env.PORT || 6000;
@@ -9,35 +10,40 @@ const server = app.listen(PORT, () => {
 });
 //====================================================================================================
 //to wrtie at terminal what methoud used
-const morgan = require('morgan');
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+const morgan = require("morgan");
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
   console.log(`mode: ${process.env.NODE_ENV}`);
 }
 //====================================================================================================
 //to call DB
-const dbConnection = require('./config/DB');
+const dbConnection = require("./config/DB");
+
 dbConnection();
 //====================================================================================================
 // Middlewares
-app.use(express.json({ limit: '20kb' }));//use to convert string from database to json file
+app.use(express.json({ limit: "20kb" })); //use to convert string from database to json file
 //====================================================================================================
 //call routes
-const categoryRoute = require('./routes/categoryRoute');
+const categoryRoute = require("./routes/categoryRoute");
 const { rawListeners } = require("./models/catigoryModel");
-app.use('/api/v1/catigories', categoryRoute);
+
+app.use("/api/v1/catigories", categoryRoute);
 //====================================================================================================
 //search at all rout previos it not exist send this message
 //create error and send to handel middilware to show
-const ApiError = require('./utils/apiError');
-app.all('*', (req, res, next) => {
+const ApiError = require("./utils/apiError");
+
+app.all("*", (req, res, next) => {
   /* const err = new Error(`Can't find this route: ${req.originalUrl}`);
   next(err.message); */
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
 });
 //====================================================================================================
 //Global error handling middelware in express to handel the error to make it error easy to read
-const globalError = require('./middlewares/errorMiddleware');
+const globalError = require("./middlewares/errorMiddleware");
+
 app.use(globalError);
 
 /* app.use((err , req , res , next) => {
@@ -54,7 +60,7 @@ app.use(globalError);
 }) */
 //====================================================================================================
 // Handle rejection outside express if you forge .catch in any promises function
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   console.error(`UnhandledRejection Errors: ${err.name} | ${err.message}`);
   server.close(() => {
     console.error(`Shutting down....`);
@@ -66,5 +72,3 @@ process.on('unhandledRejection', (err) => {
 //====================================================================================================
 //====================================================================================================
 //====================================================================================================
-
-
